e8baa71dad159789b70dc07270d6b556810cd361
PantiraR818 <PantiraPloy818@gmail.com>
2021-12-24 17:23:19 +0700

    first commit

---
+++ src/main/java/nvc/it/test101/controller/FrontEndController.java
@@ -0,0 +1,31 @@
+package nvc.it.test101.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontEndController {
+    String viewPath = "frontend/";
+
+    @GetMapping("/")
+    public String index(){
+        return viewPath+"index";
+    }
+
+    @GetMapping("/product")
+    public String product(){
+        return viewPath+"product";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return viewPath+"about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return viewPath+"contact";
+    }
+     
+
+}
